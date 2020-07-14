72ec492517d6ff90fa40ac826f172a1632143656
cleanrq <qbyun130@163.com>
2020-07-14 16:07:18 +0800

    import

--- src/main/java/com/qby/controller/BookController.java
+++ src/main/java/com/qby/controller/BookController.java
@@ -1,10 +1,22 @@
 package com.qby.controller;
 
+import com.qby.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/book")
 @Controller
 public class BookController {
+    @Autowired
+    private BookService bookService;
     public BookController() {
         System.out.println("BookController");
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println(bookService);
+        return "success";
     }
 }
